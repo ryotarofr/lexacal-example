e973f7ddd59831ef0b4d42f2f4766c4edc05e7ad
ryotarofr <ryoryo.fr0608@icloud.com>
2023-09-28 12:15:13 +0900

    init

---
+++ Editor/text-editor.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+/* Lexical Design System */
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
+import { ListItemNode, ListNode } from "@lexical/list";
+import { CodeHighlightNode, CodeNode } from "@lexical/code";
+import { AutoLinkNode, LinkNode } from "@lexical/link";
+import { TRANSFORMERS } from "@lexical/markdown";
+import ExampleTheme from "./themes/ExampleTheme"
+
+/* Lexical Plugins Local */
+import ToolbarPlugin from "./plugins/ToolbarPlugin"
+import CodeHighlightPlugin from "./plugins/CodeHighlightPlugin"
+import AutoLinkPlugin from "./plugins/AutoLinkPlugin"
+
+/* Lexical Plugins Remote */
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin";
+import { ListPlugin } from "@lexical/react/LexicalListPlugin";
+import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
+import { TabIndentationPlugin } from "@lexical/react/LexicalTabIndentationPlugin";
+
+/* Lexical Others */
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+// import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
+
+
+/* Lexical Nodes */
+// import { $getRoot, $getSelection } from "lexical";
+
+/* other package, components */
+import { Toaster } from "react-hot-toast";
+
+
+
+function Placeholder() {
+    return <div className="editor-placeholder">Enter some rich text...</div>;
+}
+
+
+export function Editor() {
+    const editorConfig = {
+        theme: ExampleTheme,
+        namespace: "daily-standup-editor",
+        onError(error: unknown) {
+            throw error;
+        },
+        nodes: [
+            HeadingNode,
+            ListNode,
+            ListItemNode,
+            QuoteNode,
+            CodeNode,
+            CodeHighlightNode,
+            TableNode,
+            TableCellNode,
+            TableRowNode,
+            AutoLinkNode,
+            LinkNode
+        ],
+    };
+
+    return (
+        <LexicalComposer initialConfig={editorConfig}>
+            <div className="editor-container">
+                <ToolbarPlugin />
+                <div className="editor-inner">
+                    <RichTextPlugin
+                        contentEditable={<ContentEditable className="editor-input" />}
+                        placeholder={<Placeholder />}
+                        ErrorBoundary={LexicalErrorBoundary}
+                    />
+                    <ListPlugin />
+                    <HistoryPlugin />
+                    <AutoFocusPlugin />
+                    <CodeHighlightPlugin />
+                    <LinkPlugin />
+                    <TabIndentationPlugin />
+                    <AutoLinkPlugin />
+                    <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+
+                    <Toaster
+                        position="top-center"
+                        reverseOrder={false}
+                    />
+                </div>
+            </div>
+        </LexicalComposer>
+    );
+}
